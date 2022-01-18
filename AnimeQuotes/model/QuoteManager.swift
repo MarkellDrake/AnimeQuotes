3da97d8b209af15aa6797caae17ae6e0e78e645b
Markell Drake <markelldrake99@icloud.com>
2022-01-18 10:00:03 -0800

    input API URL

---
+++ AnimeQuotes/model/QuoteManager.swift
@@ -0,0 +1,14 @@
+//
+//  QuoteManager.swift
+//  AnimeQuotes
+//
+//  Created by Markell Drake on 1/18/22.
+//
+
+import Foundation
+
+struct AnimeQuote {
+    
+    let animeQuote = " https://animechan.vercel.app/api/random "
+    
+}
